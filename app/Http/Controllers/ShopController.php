d788fc100782e9c36a3a4a284e7c14d72c08d27a
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2025-11-14 13:18:29 +0100

    Add product sorting and page size options on shop page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,9 +7,36 @@ use App\Models\Product;
 
 class ShopController extends Controller
 {
-    public function index(){
-        $products = Product::orderBy('created_at','DESC')->paginate(12);
-        return view('shop', compact('products'));
+    public function index(Request $request){
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_columnd = "";
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+    
+        switch($order){
+            case 1:
+                $o_columnd = 'created_at';
+                $o_order = 'DESC';
+                break;
+            case 2:
+                $o_columnd = 'created_at';
+                $o_order = 'ASC';
+                break;
+            case 3:
+                $o_columnd = 'sale_price';
+                $o_order = 'ASC';
+                break;
+            case 4:
+                $o_columnd = 'sale_price';
+                $o_order = 'DESC';
+                break;
+            default:
+                $o_columnd = 'id';
+                $o_order = 'DESC';
+        }
+        
+        $products = Product::orderBy($o_columnd,$o_order)->paginate($size);
+        return view('shop', compact('products','size','order'));
     }
 
     public function product_details($product_slug){
