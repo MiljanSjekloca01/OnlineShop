026ca995ea59902fa628da12ffe0a0d5741c919d
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2025-11-24 19:31:10 +0100

    Add admin coupon listing and creation page

--- routes/web.php
+++ routes/web.php
@@ -60,5 +60,8 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::put('/admin/product/update',[AdminController::class,'product_update'])->name('admin.product.update');
     Route::delete('/admin/product/{id}/delete',[AdminController::class,'product_delete'])->name('admin.product.delete');
 
-
+    // Coupons
+    Route::get('/admin/coupons',[AdminController::class,'coupons'])->name('admin.coupons');
+    Route::get('/admin/coupon/add',[AdminController::class,'coupon_add'])->name('admin.coupon.add');
+    Route::post('/admin/coupon/store',[AdminController::class,'coupon_store'])->name('admin.coupon.store');
 });
