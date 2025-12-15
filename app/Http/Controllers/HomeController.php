fabcbf20f07ab9231cf5e3dbab3383d91bebe26c
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2025-12-15 23:04:58 +0100

    feat: show featured products on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
         $slides = Slide::where('status',1)->take(3)->get();
         $categories = Category::orderBy('name')->get();
         $products_on_sale = Product::whereNotNull('sale_price')->where('sale_price','<>','')->inRandomOrder()->take(8)->get();
-        return view('index',compact('slides','categories','products_on_sale'));
+        $featured_products = Product::where('featured',1)->take(8)->get();
+        return view('index',compact('slides','categories','products_on_sale','featured_products'));
     }
 }
